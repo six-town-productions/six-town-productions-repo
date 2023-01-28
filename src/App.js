53173fb13b62b499647d2f339b31cd251969835d
aabenedict <anjalee.benedict@gmail.com>
2023-01-28 01:16:10 -0500

    initialized react app and added partials, assets, music, fonts, and components folders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// Import SCSS
+import './App.scss';
+// Import Links, Routes & Hooks
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+// Import Components
+import Nav from './component/Nav';
+import Header from './component/Header';
+import Player from './component/Player';
+import About from './component/About';
+import Footer from "./component/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Add Nav component with props */}
+      <Nav cartQuantity={cartQuantity} />
+
+      <Header/>
+      <Player/>
+      <About/>
+
+      {/* Create routes for all the components */}
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/player" element={<Player />} />
+        {/* Pass down state function setCartQuantity to Cart */}
+        <Route path="/about" element={<About setCartQuantity={setCartQuantity} />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      {/* Add Footer component */}
+      <Footer />
     </div>
   );
 }
