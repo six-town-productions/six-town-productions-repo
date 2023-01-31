bfea9a53de1cdddff32be4fd26763439e7e8b002
aabenedict <anjalee.benedict@gmail.com>
2023-01-31 16:37:42 -0500

    updated bugs in Player component

--- src/component/Player.js
+++ src/component/Player.js
@@ -43,36 +43,59 @@ const musicList = [
 ];
 
 const Player = () => {
-    const [isPlaying, setIsPlaying] = useState(false)
-    const [isRandom, setIsRandom] = useState(false)
     const [trackIndex, setTrackIndex] = useState(0)
     const [currTrack, setCurrTrack] = useState({})
+    const [currentTime, setCurrentTime] = useState(0);
+    const [duration, setDuration] = useState(0);
+
+    const [isPlaying, setIsPlaying] = useState(false)
+    const [isRandom, setIsRandom] = useState(false)
     const [currentSong, setCurrentSong] = useState({
         src: currTrack.audio,
         loop: false
     });
-    const [currentTime, setCurrentTime] = useState(0);
-    const [duration, setDuration] = useState(0);
+    
     const audioRef = useRef(null)
     const sliderRef = useRef(null);
 
     useEffect(() => {
-        const audio = audioRef.current;
-        audio.addEventListener('timeupdate', handleTimeUpdate);
-        return () => {
-            audio.removeEventListener('timeupdate', handleTimeUpdate);
-        };
-    }, []);
+        setCurrTrack(musicList[trackIndex]);
+    }, [trackIndex])
 
-    const handleTimeUpdate = () => {
-        const audio = audioRef.current;
+    useEffect(() => {
+        if (isPlaying) {
+            audioRef.current.play()
+        } else {
+            audioRef.current.pause()
+        }
+    }, [isPlaying])
+
+    useEffect(() => {
+        if (audioRef.current) {
+            audioRef.current.ontimeupdate = () => {
+                let time = audioRef.current.currentTime;
+                const audio = audioRef.current;
+                const slider = sliderRef.current;
+                if (time === 0) {
+                    let totalTime = formatTime(0)
+                    setDuration(totalTime)
+                    slider.value = 0;
+                } else {
+                    let totalTime = formatTime(audioRef.current.duration)
+                    setDuration(totalTime)
+                    slider.value = audio.currentTime / audio.duration;
+                }
+                setCurrentTime(formatTime(time))
+            }
+        }
+    }, [audioRef])
+
+    useEffect(() => {
+        setCurrentTime(formatTime(0))
+        setDuration(formatTime(0))
         const slider = sliderRef.current;
-        slider.value = audio.currentTime / audio.duration;
-    };
-
-    const seekTo = (e) => {
-        audioRef.current.currentTime = e.target.value * audioRef.current.duration;
-    };
+        slider.value = 0;
+    }, []);
 
     const formatTime = (seconds) => {
         const minutes = Math.floor(seconds / 60).toString().padStart(2, '0');
@@ -80,77 +103,53 @@ const Player = () => {
         return `${minutes}:${remainingSeconds}`;
     };
 
-    function reset() {
-        const time = '00:00';
-        const totalTime = '00:00';
-        const slider = sliderRef.current;
-        setCurrentTime(time);
-        setDuration(totalTime);
-        slider.value = 0;
-    }
-
-    useEffect(() => {
-        if (audioRef.current) {
-            audioRef.current.ontimeupdate = () => {
-                let time = formatTime(audioRef.current.currentTime)
-                let totalTime = formatTime(audioRef.current.duration)
-                setCurrentTime(time)
-                setDuration(totalTime)
-            }
-        }
-    }, [audioRef])
-
-    useEffect(() => {
-        setCurrTrack(musicList[trackIndex]);
-        reset();
-    }, [trackIndex])
+    const seekTo = (e) => {
+        audioRef.current.currentTime = e.target.value * audioRef.current.duration;
+    };
 
     function playPause() {
-        if (!isPlaying) {
-            audioRef.current.play()
-            setIsPlaying(!isPlaying)
-        } else {
-            audioRef.current.pause()
-            setIsPlaying(!isPlaying)
-        }
-    }
-
-    function nextTrack() {
-        reset();
-        if (currentSong.loop) {
-            setTrackIndex(trackIndex)
-        } else {
-            if (trackIndex < musicList.length - 1 && isRandom === false) {
-                setTrackIndex(trackIndex + 1)
-            } else if (trackIndex < musicList.length - 1 && isRandom === true) {
-                let randomIndex = Number.parseInt(Math.random() * musicList.length);
-                setTrackIndex(randomIndex);
-            }
-            else {
-                setTrackIndex(0);
-            }
-        }
         setIsPlaying(!isPlaying)
     }
-
+    function nextTrack() {
+        if (currentSong.loop) {
+            setCurrentSong({
+                ...currentSong,
+                loop: false
+            });
+        }
+        if (trackIndex < musicList.length - 1 && isRandom === false) {
+            setTrackIndex(trackIndex + 1)
+        } else if (trackIndex < musicList.length - 1 && isRandom === true) {
+            let randomIndex = Number.parseInt(Math.random() * musicList.length);
+            setTrackIndex(randomIndex);
+        } else {
+            setTrackIndex(0);
+        }
+        setIsPlaying(false)
+    }
     function prevTrack() {
         if (currentSong.loop) {
-            setTrackIndex(trackIndex)
-        } else {
-            if (trackIndex === 0) {
-                setTrackIndex(musicList.length - 1)
-            } else {
-                setTrackIndex(trackIndex - 1);
-            }
+            setCurrentSong({
+                ...currentSong,
+                loop: false
+            });
         }
-        setIsPlaying(!isPlaying)
+        if (trackIndex === 0) {
+            setTrackIndex(musicList.length - 1)
+        } else {
+            setTrackIndex(trackIndex - 1);
+        }
+        setIsPlaying(false)
     }
-
     function randomTrack() {
+        setCurrentSong({
+            ...currentSong,
+            loop: false
+        });
         setIsRandom(!isRandom)
     }
-
     function repeatTrack() {
+        setIsRandom(false)
         setCurrentSong({
             ...currentSong,
             loop: !currentSong.loop
@@ -173,8 +172,9 @@ const Player = () => {
                 </ul>
                 <ul className="sliderContainer">
                     <li className="currentTime">{currentTime}</li>
-                    <li><input ref={sliderRef} className="seekSlider" type="range" min="0" max="1" step="0.01" onClick={seekTo}
-                    /></li>
+                    <li>
+                        <input ref={sliderRef} className="seekSlider" type="range" min="0" max="1" step="0.01" onClick={seekTo}/>
+                    </li>
                     <li className="totalDuration">{duration}</li>
                 </ul>
                 <ul className="buttonsPlayer">
@@ -197,8 +197,9 @@ const Player = () => {
                 <ul className="sliderContainerVolume">
                     <ul>
                         <li><i className="fa-solid fa-volume-low"></i></li>
-                        <li className='volumeSliderContainer'><input type="range" min="0" max="1" step="0.01" className="volumeSlider" onChange={setVolumeTo}
-                        /></li>
+                        <li className='volumeSliderContainer'>
+                            <input type="range" min="0" max="1" step="0.01" className="volumeSlider" onChange={setVolumeTo}/>
+                        </li>
                         <li><i className="fa-solid fa-volume-high"></i></li>
                     </ul>
                     <ul>
